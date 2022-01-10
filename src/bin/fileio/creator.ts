73d7cab630a5f103e8728d38af78e60cb9debc7b
AsPulse <aspulse.x@gmail.com>
2022-01-10 14:19:39 +0900

    Add: createDirectory

--- src/bin/fileio/creator.ts
+++ src/bin/fileio/creator.ts
@@ -1,5 +1,5 @@
 import { cwd } from 'process';
-import { writeFile } from 'fs/promises';
+import { mkdir, writeFile } from 'fs/promises';
 import { dirname, resolve } from 'path';
 import {
   FileSystemError,
@@ -21,37 +21,57 @@ export class FileCreator {
   }
 
   //既にNoEntityErrorによりディレクトリを作ろうとしてから呼び出された場合、triedMakedirがtrueになります。
-  async createFile(path: string, content = '', triedMakedir = false): Promise<boolean> {
+  async createFile(
+    path: string,
+    content = '',
+    triedMakedir = false
+  ): Promise<boolean> {
     // flag: wx ... 既にパスが存在すれば失敗する
     try {
       await writeFile(resolve(this.baseDir, path), content, {
         encoding: 'utf-8',
         flag: 'wx',
       });
+      //TODO[Log]: file Created message
       return true;
     } catch (err) {
+      //新しいディレクトリをつくった時点で、同じファイルが存在する可能性もない。
+      //triedMakedir = trueでココに来たら、明らかに異常系である
+      if (triedMakedir) {
+        //TODO[Log]: Beautify the error message
+        return false;
+      }
       const error = FileSystemErrorSerialize(err);
       switch (error.code.type) {
         case 'ExistsError':
           if (await this.overrideChecker(path, error)) {
+            //TODO[Log]: file Created message
             return this.createFile(path, content);
           } else {
             return false;
           }
         case 'NoEntityError':
-          await this.createDirectory(dirname(path));
+          if (!(await this.createDirectory(dirname(path)))) {
+            return false;
+          }
           return this.createFile(path, content, true);
         case 'Unknown':
         default:
-          //TODO: Beautify the error message
+          //TODO[Log]: Beautify the error message
           console.log(error);
           return false;
       }
     }
   }
   async createDirectory(path: string): Promise<boolean> {
-    try {
-
-    }
+    return mkdir(path, { recursive: true })
+      .then(() => {
+        //TODO[Log]: directory maked
+        return true;
+      })
+      .catch(() => {
+        //TODO[Log]: directory make failed
+        return false;
+      });
   }
 }

2ec2d01b863775615ac7adb85516b3f8c1150f3b
AsPulse <aspulse.x@gmail.com>
2022-01-11 20:57:10 +0900

    Add: full colors

--- src/util/consoleCommunicater.ts
+++ src/util/consoleCommunicater.ts
@@ -6,9 +6,12 @@ interface IRGBColor {
 }
 
 const resetColor = `\x1b[0m`;
+const resetForegroundColor = `\x1b[39m`;
+const resetBackgroundColor = `\x1b[49m`;
 
-type TColorVariation = 'green' | 'blue';
+type TColorVariation = 'pink' | 'green' | 'blue';
 export const colors: { [key in TColorVariation]: IRGBColor } = {
+  pink: { r: 255, g: 0, b: 96 },
   green: { r: 0, g: 217, b: 98 },
   blue: { r: 0, g: 119, b: 255 },
 };

eec43b208c5c36255866ff8c10443f7e80eb3a7f
AsPulse <aspulse.x@gmail.com>
2022-01-12 22:17:54 +0900

    Add: more range Unicode

--- src/util/console/renderLength.ts
+++ src/util/console/renderLength.ts
@@ -1,8 +1,11 @@
+/* eslint-disable no-control-regex */
 export function renderLength(content: string): number {
   return [...content].map(v => {
     if( v.match(/^[ｦ-ﾟ]*$/) ) return 1;
-    // eslint-disable-next-line no-control-regex
-    if( v.match(/^[^\x01-\x7E\xA1-\xDF]+$/) ) return 2;
+    if( v.match(/^[\u2700-\u2bff]+$/u) ) return 1;
+    if( v.match(/^[\ufe00-\uffff]+$/u) ) return 2;
+    if( v.match(/^[\u2e00-\u9fff]+$/u) ) return 2;
+    if( v.match(/^[\u{20000}-\u{2ff00}]+$/u) ) return 2;
     return 1;
   }).reduce<number>((a, b) => a + b, 0);
 }

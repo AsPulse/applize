90e45747ae1183a1ee55cf8184b70675349f15b8
AsPulse <aspulse.x@gmail.com>
2022-01-16 23:37:51 +0900

    Add: start logo

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -3,6 +3,8 @@ import {
   colors,
   confirmYesNo,
   decorate,
+  filledBySpace,
+  say,
 } from '../util/console/consoleCommunicater';
 import { FileCreator } from './fileio/creator';
 
@@ -13,4 +15,17 @@ new FileCreator((path) =>
     path,
     ' is already exists. Override?',
   ])
+);
+
+say();
+say(
+  filledBySpace(
+    [
+      decorate(colors.white, colors.pink, true) + 'Hello Applize!',
+      decorate(colors.white, colors.pink, false) + 'for TypeScript in everything...',
+    ],
+    3,
+    1,
+    decorate(colors.white, colors.pink, true)
+  )
 );

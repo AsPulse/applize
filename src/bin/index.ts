c5346623f2b1e94ccf7d100af8a54d15e78ac145
AsPulse <84216737+AsPulse@users.noreply.github.com>
2022-01-29 19:49:57 +0900

    Feature/setup skelton (#45)
    * Add: start logo
    
    * Add: new test pattern
    
    * Fix: control-letter renderLength problem
    
    * Apply: lint
    
    * Add: symbol before message
    
    * Apply: lint

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -3,6 +3,9 @@ import {
   colors,
   confirmYesNo,
   decorate,
+  filledBySpace,
+  say,
+  symbols,
 } from '../util/console/consoleCommunicater';
 import { FileCreator } from './fileio/creator';
 
@@ -14,3 +17,20 @@ new FileCreator((path) =>
     ' is already exists. Override?',
   ])
 );
+
+say();
+say(
+  filledBySpace(
+    [
+      decorate(colors.white, colors.pink, true) +
+        symbols.hexagon +
+        ' Hello Applize!',
+      decorate(colors.white, colors.pink, false) +
+        'for TypeScript in everything...',
+    ],
+    3,
+    1,
+    decorate(colors.white, colors.pink, true)
+  )
+);
+say();

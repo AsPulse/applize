b74e4a97a16fa0b521823c7b1b29efbedb391495
AsPulse <84216737+AsPulse@users.noreply.github.com>
2022-01-30 23:05:55 +0900

    Feature/build (#49)
    * Add: with-Sourcemap
    
    * Add: baseDOMBuilder
    
    * Add: with HTMLElement type
    
    * Fix: without children
    
    * Apply: lint
    
    * Install: closure compiler
    
    * Apply: lint

--- src/client/dombuilder/develop/page.ts
+++ src/client/dombuilder/develop/page.ts
@@ -1,7 +1,22 @@
-import { IApplizeDomBuilder } from '..';
+import { IApplizeDOMBuilder } from '..';
 import { DomRenderer } from '../domrenderer';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-((_adb: IApplizeDomBuilder) => {
-  console.log('hello!');
-})(new DomRenderer(document.body));
+document.addEventListener('DOMContentLoaded', () => {
+  ((adb: IApplizeDOMBuilder) => {
+    const data = adb.build('div', (div) => {
+      return {
+        paragraph1: div('p'),
+        paragraph2: div('p'),
+        paragraph3: div('p'),
+        inner: div('div', (inner) => {
+          return { text: inner('s', (bold) => bold('b')) };
+        }),
+      };
+    });
+    data.expose.paragraph1.element.textContent = 'Lorem ipsum dolor sit amet,';
+    data.expose.paragraph2.element.textContent = 'consectetur adipiscing elit,';
+    data.expose.paragraph3.element.textContent =
+      'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+    data.expose.inner.expose.text.expose.element.textContent = 'This is inner!';
+  })(new DomRenderer(document.body));
+});
--- src/client/dombuilder/domrenderer.ts
+++ src/client/dombuilder/domrenderer.ts
@@ -1,5 +1,32 @@
-import { IApplizeDomBuilder } from '.';
+import {
+  HTMLTags,
+  IApplizeDOM,
+  IApplizeDOMBuilder,
+  TBuilder,
+  TNoExpose,
+} from '.';
 
-export class DomRenderer implements IApplizeDomBuilder {
+export class DomRenderer implements IApplizeDOMBuilder {
   constructor(public targetElement: HTMLElement) {}
+  build<T extends HTMLTags, U>(...args: TBuilder<T, U>) {
+    const element = this.parse(...args);
+    this.targetElement.appendChild(element.element);
+    return element;
+  }
+  parse<T extends HTMLTags, U = TNoExpose>(
+    ...args: TBuilder<T, U>
+  ): IApplizeDOM<HTMLElementTagNameMap[T], U> {
+    const element = document.createElement(args[0]);
+    const last = args[1];
+    if (last !== undefined) {
+      const expose = last(<S extends HTMLTags, K>(...args: TBuilder<S, K>) => {
+        const parsed = this.parse(...args);
+        element.appendChild(parsed.element);
+        return parsed;
+      });
+      return { element, expose };
+    } else {
+      return { element, expose: last as unknown as U };
+    }
+  }
 }

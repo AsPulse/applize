8d14569df6cd15c41743ee10c2bea51cc13fb3c4
AsPulse <aspulse.x@gmail.com>
2022-02-02 17:52:45 +0900

    Add: phase

--- src/builder/index.ts
+++ src/builder/index.ts
@@ -1,1 +1,12 @@
-export class ApplizeBuilder {}
+interface IApplizeBuildPhase {
+  name: string;
+  execute: () => void;
+}
+
+export class ApplizeBuilder {
+  phases: IApplizeBuildPhase[] = [];
+
+  addPhase(name: string, execute: () => void): void {
+    this.phases.push({ name, execute });
+  }
+}

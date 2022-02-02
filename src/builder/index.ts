88c6458dad62b3a103852f0efd7826b468c552fb
AsPulse <aspulse.x@gmail.com>
2022-02-02 18:25:22 +0900

    Add: promise runner

--- src/builder/index.ts
+++ src/builder/index.ts
@@ -13,4 +13,13 @@ export class ApplizeBuilder {
   addPhaseAsync(name: string, execute: () => Promise<void>): void {
     this.phases.push({ name, execute });
   }
+
+  async run() {
+    await this.phases
+    .map<{ index: number } & IApplizeBuildPhase>((v, i) => ({ index: i, ...v }))
+    .map(v => {
+      return v.execute;
+    })
+    .reduce((a, b) => () => a().then(b), () => Promise.resolve())();
+  }
 }

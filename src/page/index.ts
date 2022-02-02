9df8986e88a93c804160cb5b780d9bcbb777a0ea
AsPulse <aspulse.x@gmail.com>
2022-02-02 17:30:49 +0900

    Apply: lint

--- src/page/index.ts
+++ src/page/index.ts
@@ -4,15 +4,15 @@ export type router = (url: string) => void;
 export type render = (adb: IDOMRenderer) => void;
 
 declare const global: { fileName?: string };
-declare const window: { __applize: { render: IDOMRenderer } }| undefined;
+declare const window: { __applize: { render: IDOMRenderer } } | undefined;
 
 export class ApplizePage {
   fileName: string | null = null;
   constructor(public router: router, public render: render) {
-    if ( global.fileName ) {
+    if (global.fileName) {
       this.fileName = global.fileName;
     }
-    if ( window ) {
+    if (window) {
       render(window.__applize.render.clone());
     }
   }

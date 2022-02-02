b062428200a4e26054757a15cf99d568ec9aeb09
AsPulse <aspulse.x@gmail.com>
2022-02-02 18:12:07 +0900

    Add: phase async

--- src/builder/index.ts
+++ src/builder/index.ts
@@ -1,12 +1,16 @@
 interface IApplizeBuildPhase {
   name: string;
-  execute: () => void;
+  execute: () => Promise<void>;
 }
 
 export class ApplizeBuilder {
   phases: IApplizeBuildPhase[] = [];
 
   addPhase(name: string, execute: () => void): void {
+    this.phases.push({ name, execute: () => Promise.resolve(execute()) });
+  }
+
+  addPhaseAsync(name: string, execute: () => Promise<void>): void {
     this.phases.push({ name, execute });
   }
 }

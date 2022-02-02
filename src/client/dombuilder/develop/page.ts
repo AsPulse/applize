1be5a66429ea7894889539b4a8533e03fcf0b3ef
AsPulse <84216737+AsPulse@users.noreply.github.com>
2022-02-02 16:20:30 +0900

    Develop/sandbox (#53)
    * Remove: develop
    
    * Add: sandbox

--- src/client/dombuilder/develop/page.ts
+++
@@ -1,9 +0,0 @@
-import { DomRenderer } from '../domrenderer';
-import { content } from './content';
-
-const program = `document.addEventListener('DOMContentLoaded', () => {
-  (${content.toString()})(new (${DomRenderer.toString()})(document.body));
-})`;
-
-eval(program);
-console.log(program);

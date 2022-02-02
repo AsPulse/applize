a0776b9d8d4b52126c8a02f9ec3bfccd9e824501
AsPulse <aspulse.x@gmail.com>
2022-02-03 00:09:36 +0900

    Fix: more collectly check about window object

--- src/page/index.ts
+++ src/page/index.ts
@@ -4,15 +4,16 @@ export type router = (url: string) => void;
 export type render = (adb: IDOMRenderer) => void;
 
 declare const global: { fileName?: string };
-declare const window: { __applize: { render: IDOMRenderer } } | undefined;
+declare const window: { __applize: { render: IDOMRenderer } };
 
 export class ApplizePage {
   fileName: string | null = null;
   constructor(public router: router, public render: render) {
+    const chkFromWeb = (() => { try { if ( window !== undefined ) return true; } catch { return false; } return false;})();
     if (global.fileName) {
       this.fileName = global.fileName;
     }
-    if (window) {
+    if (chkFromWeb) {
       render(window.__applize.render.clone());
     }
   }

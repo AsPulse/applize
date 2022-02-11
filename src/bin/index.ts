6157999e673929f2ec039dc295d6b403a302c8c4
AsPulse <aspulse.x@gmail.com>
2022-02-11 20:05:27 +0900

    Add: writer

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
+import { cwd } from 'process';
 import {
   colors,
   confirmYesNo,
   decorate,
   filledBySpace,
+  input,
   say,
   symbols,
 } from '../util/console/consoleCommunicater';
+import { fileAPISchemats } from './data/file-apischemats';
+import { fileBuildjs } from './data/file-buildjs';
+import { fileEntryIndexts } from './data/file-entryIndexts';
+import { fileIndexhtml } from './data/file-indexhtml';
+import { fileIndexts } from './data/file-indexts';
+import { filePageIndexts } from './data/file-pagesIndexTs';
 import { FileCreator } from './fileio/creator';
 
-new FileCreator(path =>
-  confirmYesNo([
-    decorate(colors.green),
-    'File ',
-    path,
-    ' is already exists. Override?',
-  ])
-);
 
 say();
 say(
@@ -34,3 +34,33 @@ say(
   )
 );
 say();
+
+void (async () => {
+  say('Your current-directory: ', cwd());
+  const root = await input(['Root directory ( default is current-directory ): '], () => Promise.resolve(true));
+
+  const fc = new FileCreator(path =>
+    confirmYesNo([
+      decorate(colors.green),
+      'File ',
+      path,
+      ' is already exists. Override?',
+    ]), root
+  );
+  await fc.createDirectory('./src');
+  await fc.createDirectory('./pages');
+  await fc.createDirectory('./dist');
+  await fc.createDirectory('./entry');
+
+  await fc.createFile('./build.js', fileBuildjs);
+  await fc.createFile('./src/index.ts', fileIndexts);
+  await fc.createFile('./src/apiSchema.ts', fileAPISchemats);
+
+  await fc.createFile('./pages/index.ts', filePageIndexts);
+
+  await fc.createFile('./entry/index.html', fileIndexhtml);
+  await fc.createFile('./entry/index.ts', fileEntryIndexts);
+
+  say();
+  say(decorate(colors.white, colors.pink, true), ' DONE! ');
+})();

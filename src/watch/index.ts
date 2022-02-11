dd1c6014dad20987807d8ead14a15e5aa22b5707
AsPulse <aspulse.x@gmail.com>
2022-02-11 20:33:41 +0900

    Apply: lint

--- src/watch/index.ts
+++ src/watch/index.ts
@@ -2,27 +2,30 @@ import { exec, spawn } from 'child_process';
 import { watch } from 'chokidar';
 import { exit } from 'process';
 
-export function applizeWatch(directory: string, target: string, builder: string) {
+export function applizeWatch(
+  directory: string,
+  target: string,
+  builder: string
+) {
   const watcher = watch(directory, {
-    persistent: true
+    persistent: true,
   });
 
   let process = spawn(`node ${target}`);
   process.stdout.on('data', data => {
-    console.log((<{toString: () => string}>data).toString());
+    console.log((<{ toString: () => string }>data).toString());
   });
 
   watcher.on('ready', () => {
-
     ['add', 'change', 'unlink'].forEach(v => {
       watcher.on(v, () => {
         exec(`node ${builder}`, (error, stdout) => {
-          if(error) exit(1);
+          if (error) exit(1);
           console.log(stdout);
           process.kill();
           process = spawn(`node ${target}`);
           process.stdout.on('data', data => {
-            console.log((<{toString: () => string}>data).toString());
+            console.log((<{ toString: () => string }>data).toString());
           });
         });
       });

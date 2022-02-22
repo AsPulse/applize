4e6926f363d752415430da2e70634495e484457a
AsPulse <84216737+AsPulse@users.noreply.github.com>
2022-02-23 08:25:22 +0900

    Fix/erase classlist (#145)
    * Add: erase process
    
    * Apply: lint

--- src/clientBase/index.ts
+++ src/clientBase/index.ts
@@ -56,6 +56,11 @@ export function ClientInitialize(applizeRoot: string) {
         if (!renderedTarget) return;
 
         const cloned = renderedTarget.cloneNode(false);
+        if (cloned instanceof HTMLElement) {
+          [...cloned.classList]
+            .filter(v => /^style-page-([0-9]+)$/.test(v))
+            .map(v => cloned.classList.remove(v));
+        }
         const fragment = document.createDocumentFragment();
 
         const pageScript = document.createElement('script');

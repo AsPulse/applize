ebf712dd454ef3a81c08e6dd49a76d7ebf584dc4
AsPulse <84216737+AsPulse@users.noreply.github.com>
2022-05-03 14:50:36 +0900

    Fix/logger dont include options (#180)
    * Add: options
    
    * Apply: lint

--- src/applize/index.ts
+++ src/applize/index.ts
@@ -122,12 +122,14 @@ export class Applize<
       trailingSlash: options.trailingSlash ?? 'NoChange',
       rootEndPoint: options.rootEndPoint ?? { url: ['applize'] },
       distRoot: options.distRoot ?? cwd(),
-      logger: data =>
-        console.log(
-          `(${Math.round(data.time * 100) / 100}ms) ${data.code} ${data.url} ${
-            data.remoteAddress ?? ''
-          }`
-        ),
+      logger:
+        options.logger ??
+        (data =>
+          console.log(
+            `(${Math.round(data.time * 100) / 100}ms) ${data.code} ${
+              data.url
+            } ${data.remoteAddress ?? ''}`
+          )),
     };
 
     server.on('request', (req, res) => {

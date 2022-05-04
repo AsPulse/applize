43cde927eb08deb19f96b9876ed1361e53a4f502
AsPulse <aspulse.x@gmail.com>
2022-05-04 13:20:36 +0900

    Apply: lint

--- src/watch/index.ts
+++ src/watch/index.ts
@@ -1,4 +1,4 @@
-import type { ChildProcessWithoutNullStreams} from 'child_process';
+import type { ChildProcessWithoutNullStreams } from 'child_process';
 import { spawn } from 'child_process';
 import { watch } from 'chokidar';
 import { createHash } from 'crypto';
@@ -17,40 +17,46 @@ export function applizeWatch(
 
   let targetProcess: null | ChildProcessWithoutNullStreams = null;
 
-  new Watcher(directory, builder, target, () => new Promise(resolve => {
-    const childProcess = spawn('node', [builder]);
-    childProcess.stdout.on('data', chunk => {
-      process.stdout.write(chunkLog(chunk));
-    });
-    childProcess.stderr.on('data', chunk => {
-      process.stdout.write(chunkLog(chunk));
-    });
-    childProcess.on('exit', code => {
-      resolve(code === 0);
-    })
-  }), async () => {
-    await (new Promise<void>(resolve => {
-      if(targetProcess !== null) {
-        targetProcess.on('exit', () => {
-          resolve();
+  new Watcher(
+    directory,
+    builder,
+    target,
+    () =>
+      new Promise(resolve => {
+        const childProcess = spawn('node', [builder]);
+        childProcess.stdout.on('data', chunk => {
+          process.stdout.write(chunkLog(chunk));
         });
-        targetProcess.kill('SIGTERM');
-      } else {
-        resolve();
-      }
-    }));
+        childProcess.stderr.on('data', chunk => {
+          process.stdout.write(chunkLog(chunk));
+        });
+        childProcess.on('exit', code => {
+          resolve(code === 0);
+        });
+      }),
+    async () => {
+      await new Promise<void>(resolve => {
+        if (targetProcess !== null) {
+          targetProcess.on('exit', () => {
+            resolve();
+          });
+          targetProcess.kill('SIGTERM');
+        } else {
+          resolve();
+        }
+      });
 
-    targetProcess = null;
+      targetProcess = null;
 
-    targetProcess = spawn('node', [target]);
-    targetProcess.stdout.on('data', chunk => {
-      process.stdout.write(chunkLog(chunk));
-    });
-    targetProcess.stderr.on('data', chunk => {
-      process.stdout.write(chunkLog(chunk));
-    });
-  });
-
+      targetProcess = spawn('node', [target]);
+      targetProcess.stdout.on('data', chunk => {
+        process.stdout.write(chunkLog(chunk));
+      });
+      targetProcess.stderr.on('data', chunk => {
+        process.stdout.write(chunkLog(chunk));
+      });
+    }
+  );
 }
 export function chunkLog(chunk: unknown): string {
   try {
@@ -60,63 +66,75 @@ export function chunkLog(chunk: unknown): string {
   }
 }
 export class Watcher {
-
   changes = 0;
   doublingThreshold = 300;
   refreshing = false;
 
-  constructor(public directory: string, builder: string, public target: string, public refresher: () => Promise<boolean>, public reExecutor: () => Promise<void>) {
-    watch(directory, { ignored: [builder, dirname(target), /node_modules/, /\.git/] }).on('all', () => {
-      if(this.refreshing) return;
+  constructor(
+    public directory: string,
+    builder: string,
+    public target: string,
+    public refresher: () => Promise<boolean>,
+    public reExecutor: () => Promise<void>
+  ) {
+    watch(directory, {
+      ignored: [builder, dirname(target), /node_modules/, /\.git/],
+    }).on('all', () => {
+      if (this.refreshing) return;
       const ourChange = ++this.changes;
       setTimeout(() => {
-        if ( ourChange !== this.changes ) return;
+        if (ourChange !== this.changes) return;
         void this.refresh();
       }, this.doublingThreshold);
     });
-
   }
 
   async refresh() {
-    if(this.refreshing){
+    if (this.refreshing) {
       return;
     }
     this.refreshing = true;
     const beforeSnapshot = await getSnapShot(this.directory, this.target);
-    if(await this.refresher()) {
+    if (await this.refresher()) {
       void this.reExecutor();
     }
     const afterSnapshot = await getSnapShot(this.directory, this.target);
     this.refreshing = false;
-    if ( !equalSnapshot(beforeSnapshot, afterSnapshot) ) {
+    if (!equalSnapshot(beforeSnapshot, afterSnapshot)) {
       void this.refresh();
     }
   }
 }
 
 type Snapshot = {
-  digest: string,
-  path: string
+  digest: string;
+  path: string;
 };
-export async function getSnapShot(directory: string, target: string): Promise<Snapshot[]> {
+export async function getSnapShot(
+  directory: string,
+  target: string
+): Promise<Snapshot[]> {
   const basedir = dirname(target);
   const hashes = (await getAllFilesInDir(directory, []))
     .filter(v => {
-      if(v.directory.startsWith(basedir)) return false;
+      if (v.directory.startsWith(basedir)) return false;
       return true;
     })
-    .map(v =>  new Promise<Snapshot>(promiseResolve => {
-      const shasum = createHash('sha1');
-      const path = resolve(v.directory, v.dirent.name);
-      const stream = createReadStream(path);
-      stream.on('data', chunk => shasum.update(chunk));
-      stream.on('close', () =>
-        promiseResolve({
-          digest: shasum.digest('hex'),
-          path
-        }
-      ));
-    }));
+    .map(
+      v =>
+        new Promise<Snapshot>(promiseResolve => {
+          const shasum = createHash('sha1');
+          const path = resolve(v.directory, v.dirent.name);
+          const stream = createReadStream(path);
+          stream.on('data', chunk => shasum.update(chunk));
+          stream.on('close', () =>
+            promiseResolve({
+              digest: shasum.digest('hex'),
+              path,
+            })
+          );
+        })
+    );
   return Promise.all(hashes);
 }
 
@@ -124,8 +142,13 @@ export function equalSnapshot(a: Snapshot[], b: Snapshot[]) {
   const aKeys = a.map(v => v.path);
   const bKeys = b.map(v => v.path);
   const keys = [...new Set([...aKeys, ...bKeys])];
-  if(keys.filter(v => !aKeys.includes(v)).length > 0 || keys.filter(v => !bKeys.includes(v)).length > 0) {
+  if (
+    keys.filter(v => !aKeys.includes(v)).length > 0 ||
+    keys.filter(v => !bKeys.includes(v)).length > 0
+  ) {
     return false;
   }
-  return (keys.every(v => a.find(e => e.path === v)?.digest === b.find(e => e.path === v)?.digest));
+  return keys.every(
+    v => a.find(e => e.path === v)?.digest === b.find(e => e.path === v)?.digest
+  );
 }
